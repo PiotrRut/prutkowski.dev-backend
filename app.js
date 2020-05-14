a167903be5f24c1665ccde845c01cbb51953a436
PiotrRut <piotr.rutkowski@city.ac.uk>
2020-05-15 00:10:21 +0200

    Moved all routes to a separate file with router export

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const cookieParser = require('cookie-parser')
 const app = express();
 const port =  process.env.PORT || 3001
 const path = require("path");
-const { BlobServiceClient, StorageSharedKeyCredential } = require("@azure/storage-blob");
+const gallery = require('./routes/gallery')
 
 // Set the view engine to ejs for index page rendering
 app.set('view engine', 'ejs');
+
+app.use(cookieParser());
 
 // CORS (Cross-Origin Resource Sharing) config, preventing violations in the future
 app.use(function (req, res, next) {
@@ -17,42 +19,16 @@ app.use(function (req, res, next) {
   next()
 })
 
-app.use(cookieParser());
+// Import the gallery routes
+app.use('/gallery', gallery)
 
+// Render index.ejs on root endpoint (/)
 app.get('/', (req, res) => {
   res.render('pages/index');
 })
 
-// Create a new url to access the image with the image file name appended at the end
-function genImageUrl(key){
-  return `https://prutkowskigallery.blob.core.windows.net/gallery/${key}`
-}
 
-// Endpoint responsible for returning full-res and low-res URL for each picture (blob)
-app.get('/getAllImages', async (req, res) => {
-  const response = []
-  let urls = []
-    const blobSasUrl = `https://${process.env.AZURE_SPACE}.blob.core.windows.net/?${process.env.AZURE_TOKEN}`
-    // Connect to a new BlobServiceClient
-    const blobServiceClient = new BlobServiceClient(blobSasUrl);
-    // Get a container client from the BlobServiceClient
-    const containerClient = blobServiceClient.getContainerClient(process.env.AZURE_CONTAINER);
-  
-    // Grab all file names from the container and append to "urls"
-    for await (const blob of containerClient.listBlobsFlat()) {
-      urls.push(blob.name)
-    };
-    // Sort the files into low-res and high-res links
-    urls = urls.filter(photo => photo.includes('-compressed'))
-      urls.forEach(name  => {
-        image = {
-          lowRes: genImageUrl(name),
-          highRes: genImageUrl(name).replace('-compressed', '')
-        }
-      response.push(image)
-      })
-    res.status(200).send(response)
-})
+
 
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
---
+++ routes/gallery.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const express = require('express')
+const router = express.Router();
+const { BlobServiceClient, StorageSharedKeyCredential } = require("@azure/storage-blob");
+
+// Create a new url to access the image with the image file name appended at the end
+function genImageUrl(key){
+  return `https://prutkowskigallery.blob.core.windows.net/gallery/${key}`
+}
+  
+// Endpoint responsible for returning full-res and low-res URL for each picture (blob)
+router.get('/getAllPhotos', async (req, res) => {
+  const response = []
+  let urls = []
+  const blobSasUrl = `https://${process.env.AZURE_SPACE}.blob.core.windows.net/?${process.env.AZURE_TOKEN}`
+
+  // Connect to a new BlobServiceClient
+  const blobServiceClient = new BlobServiceClient(blobSasUrl);
+  // Get a container client from the BlobServiceClient
+  const containerClient = blobServiceClient.getContainerClient(process.env.AZURE_CONTAINER);
+
+  // Grab all file names from the container and append to "urls"
+  for await (const blob of containerClient.listBlobsFlat()) {
+  urls.push(blob.name)
+  };
+  // Sort the files into low-res and high-res links
+  urls = urls.filter(photo => photo.includes('-compressed'))
+  urls.forEach(name  => {
+      image = {
+      lowRes: genImageUrl(name),
+      highRes: genImageUrl(name).replace('-compressed', '')
+      }
+  response.push(image)
+  })
+  res.status(200).send(response)
+})
+
+module.exports = router;
